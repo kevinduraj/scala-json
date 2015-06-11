dbefda037657fd367192794b1982ead3b60cdbdf
Kevin Duraj <kevin.duraj@mdinsider.com>
2015-06-11 11:44:58 -0700

    Person class

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "scala-json"
+
+version := "1.1"
+
+//scalaVersion := "2.11.6"
+scalaVersion := "2.10.5"
+
+//libraryDependencies += "com.typesafe.play" % "play_2.10" % "2.4.0"
+//libraryDependencies += "io.spray" %%  "spray-json" % "1.3.2"
---
+++ src/main/scala/ReadJson.scala
@@ -0,0 +1,19 @@
+/**
+ * Created by kduraj on 6/11/15.
+ */
+
+object ReadJson {
+
+  def main(args: Array[String]) {
+
+    /*-------------------------------------*/
+    println("Parse Person JSON ... ")
+    val person = new Person
+    person.readJson()
+    /*-------------------------------------*/
+
+    
+
+  }
+
+}
